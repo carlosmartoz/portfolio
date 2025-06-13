a63283c3c19fe34a91e6e2739b72ce8994bc54db
Carlos <carlosmrtzo@gmail.com>
2025-06-13 14:38:14 -0300

    remove comments

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-// Next config
 import type { NextConfig } from "next";
-
-// Next intl
 import createNextIntlPlugin from "next-intl/plugin";
 
-// Next intl plugin
 const withNextIntl = createNextIntlPlugin();
-
-// Next config
 const nextConfig: NextConfig = {};
 
-// Export
 export default withNextIntl(nextConfig);
